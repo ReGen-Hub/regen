a4bbf2208ea33e35135429103eb7019e18246a9d
Purva Chaudhari <purvachaudhari@Purvas-MacBook-Pro.local>
2023-03-24 01:28:41 +0530

    blob

--- src/components/ecommerce/AddProduct.tsx
+++ src/components/ecommerce/AddProduct.tsx
@@ -8,14 +8,35 @@ const data = [
   "Price",
   "Corbon Footprint",
 ];
+function makeFileObjects () {
+  // You can create File objects from a Blob of binary data
+  // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+  // Here we're just storing a JSON object, but you can store images,
+  // audio, or whatever you want!
+  const obj = { hello: 'world' }
+  const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' })
+
+  const files = [
+    new File(['contents-of-file-1'], 'plain-utf8.txt'),
+    new File([blob], 'hello.json')
+  ]
+  return files
+}
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDk4MDU1NmE5NzM0RTkyNGJGRDFkNjA4QjA1QTk3OGIyQmY2RjhkMWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Nzk1OTQ3Nzc2ODIsIm5hbWUiOiJSZWdlbiJ9.4G_gD1y-HgGUcGi0qMXvybZoNfeuuitK0w0PWSfi63E"
 //const token = process.env.API_TOKEN
 const client = new Web3Storage({ token })
 console.log(token , "checking token")
 
 async function storeFiles (file: any) {
-  console.log("In store files befre", file)
-  const files = [file]
+  const [data, setData] = useState({});
+  fetch(file)
+    .then(res => res.blob())
+    .then(blob => {
+      const file = new File([blob], 'dot.png', blob)
+  console.log(file)
+})
+  console.log("In store files befre")
+  const files = data
 
   const cid = await client.put(files)
   console.log("In store files : ", cid)
@@ -37,8 +58,8 @@ export default function AddProduct() {
 
 
 const SaveData = () => {
-  console.log("fromData", fromData.image);
-  uploadCoverImage(fromData.image);
+  console.log("fromData", fromData.Image);
+  uploadCoverImage(fromData.Image);
 }
 function imageUploaded(e: any, fieldName: any) {
   var file = e.target.files[0];
@@ -61,11 +82,11 @@ function imageUploaded(e: any, fieldName: any) {
 }
 
 const uploadCoverImage = async (coverImage: any) => {
-  console.log("Uploading Cover Image...", coverImage);
+  console.log("Uploading Cover Image...");
 
   try {
-    
-    const image = await storeFiles(coverImage); 
+    const file = "/Users/purvachaudhari/Downloads/image.png"
+    const image = await storeFiles(file); 
     console.log(image)
   } catch (err) {
     console.log("Error Uploading Cover Image", err);
